f59ced18bd8b29c34582612f9dad8487aeccc7b8
Igor <Aleshichev.git@email.ua>
2025-07-09 12:47:45 +0300

    added celery worker

--- app/celery_app.py
+++ app/celery_app.py
@@ -4,8 +4,12 @@ celery_app = Celery(
     "worker",
     broker="redis://redis:6379/0",      # 🔁 Redis — как в docker-compose
     backend="redis://redis:6379/1",     # 👈 можно использовать для хранения результата
+    include=["app.tasks.notify"],
 )
 
-celery_app.conf.task_routes = {
-    "app.tasks.*": {"queue": "main-queue"},
-}
+celery_app.conf.update(
+    worker_hostname="worker123",  
+    task_routes={
+        "app.tasks.*": {"queue": "main-queue"},
+    },
+)
--- app/core/database.py
+++ app/core/database.py
@@ -1,16 +1,31 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncEngine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    AsyncEngine,
+)
 from sqlalchemy.orm import sessionmaker
-
+from contextlib import asynccontextmanager
 from app.core.config import settings
 
-engine: AsyncEngine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+engine: AsyncEngine = create_async_engine(
+    settings.DATABASE_URL, echo=False, future=True
+)
 
-async_session = async_sessionmaker(engine, expire_on_commit=False, autoflush=False, autocommit=False)
+async_session = async_sessionmaker(
+    engine, expire_on_commit=False, autoflush=False, autocommit=False
+)
+
 
 async def dispose():
     await engine.dispose()
 
+
 async def get_db():
     async with async_session() as session:
         yield session
-        
+
+@asynccontextmanager
+async def get_db_context():
+    async with async_session() as session:
+        yield session
